474b477bba2bf5457fcbf138f8f77f15b0dabf85
bugsw <bugswaterloo@gmail.com>
2024-10-09 15:34:16 -0400

    Default Message

--- git-push.bat
+++ git-push.bat
@@ -2,6 +2,6 @@ git init
 git add .
 git commit -m "Default Message"
 git remote add https://github.com/bugs54/MSE---Year-2---Project.git
-git branch Marsh
-git push origin Marsh
+git branch Wow
+git push origin Wow
 pause
